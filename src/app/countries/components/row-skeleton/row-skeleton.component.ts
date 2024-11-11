ca15c5d7179c3b2d13033b45add6087c47e5bf7a
CarlosPProjects <cg9822@gmail.com>
2024-11-12 00:58:47 +0100

    Refactor table component to handle empty data and display a message when no data is found

---
+++ src/app/countries/components/row-skeleton/row-skeleton.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-row-skeleton',
+  standalone: true,
+  imports: [],
+  templateUrl: './row-skeleton.component.html',
+  styles: ``
+})
+export class RowSkeletonComponent {
+
+}
--- src/app/countries/components/table/table.component.ts
+++ src/app/countries/components/table/table.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { LucideAngularModule, Image, Info } from 'lucide-angular';
+import { Component, Input } from '@angular/core';
+import {
+  LucideAngularModule,
+  Image,
+  Info,
+  ShieldQuestion,
+} from 'lucide-angular';
+import { RowSkeletonComponent } from '../row-skeleton/row-skeleton.component';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, RowSkeletonComponent],
   templateUrl: './table.component.html',
 })
 export class TableComponent {
   readonly imageIcon = Image;
   readonly infoIcon = Info;
+  readonly shielQuestionIcon = ShieldQuestion;
+
+  @Input() data: any[] = [];
+  public loading: boolean = true;
 }
