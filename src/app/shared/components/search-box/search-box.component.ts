58f7f2cac5aa24ff551f6dbff315eddc96e66a02
CarlosPProjects <cg9822@gmail.com>
2024-11-11 21:16:18 +0100

    Add Tailwind CSS configuration and search box component

---
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ContainerComponent } from '../container/container.component';
+
+@Component({
+  selector: 'shared-search-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-box.component.html',
+  styles: ``
+})
+export class SearchBoxComponent {
+
+}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  endpoint: 'https://restcountries.com/v3.1/',
+};
