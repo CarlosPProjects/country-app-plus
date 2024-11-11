61ca70758748cc6820bf70221521e0cd76fe8123
CarlosPProjects <cg9822@gmail.com>
2024-11-11 21:52:19 +0100

    Refactor search box component and update styling in app component

--- src/app/components/search-box/search-box.component.ts
+++ src/app/components/search-box/search-box.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { LucideAngularModule, Search } from 'lucide-angular';
 
 @Component({
   selector: 'app-search-box',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, FormsModule],
   templateUrl: './search-box.component.html',
   styles: ``,
 })
 export class SearchBoxComponent {
   readonly SearchIcon = Search;
+
+  public _inputValue: string = '';
+
+  constructor() {}
+
+  public clearSearchInput() {
+    this._inputValue = '';
+  }
+
+  public search(): void {
+    console.log('search:', this._inputValue);
+  }
 }
