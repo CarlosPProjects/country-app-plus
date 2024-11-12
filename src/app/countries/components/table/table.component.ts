e74f7ea265241dbf836d04438f5fac2ff432f408
CarlosPProjects <cg9822@gmail.com>
2024-11-12 19:37:54 +0100

    Refactor table component to pass countries and loading state as inputs

--- src/app/countries/components/table/table.component.ts
+++ src/app/countries/components/table/table.component.ts
@@ -3,9 +3,6 @@ import { LucideAngularModule, ShieldQuestion } from 'lucide-angular';
 import { RowSkeletonComponent } from '../row-skeleton/row-skeleton.component';
 import { RowComponent } from '../row/row.component';
 import { Country } from '../../interfaces/country';
-import { CountriesService } from '../../services/countries.service';
-import { SearchService } from '../../../components/services/search-box.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-table',
@@ -13,49 +10,10 @@ import { Subscription } from 'rxjs';
   imports: [LucideAngularModule, RowSkeletonComponent, RowComponent],
   templateUrl: './table.component.html',
 })
-export class TableComponent implements OnInit, OnDestroy {
+export class TableComponent{
   readonly shieldQuestionIcon = ShieldQuestion;
 
-  searchValue: string = '';
-  private subscription!: Subscription;
+  @Input() public countries: Country[] = [];
+  @Input() public loading: boolean = true;
 
-  public loading: boolean = true;
-  public countries: Country[] = [];
-
-  constructor(
-    private countriesService: CountriesService,
-    private searchService: SearchService
-  ) {}
-
-  private searchByCapital(value: string): void {
-    this.loading = true;
-    this.countriesService.searchByCapital(value).subscribe({
-      next: (countries) => {
-        this.countries = countries;
-        this.loading = false;
-      },
-      error: (error) => {
-        console.error('Error al buscar países:', error);
-        this.countries = [];
-        this.loading = false;
-      },
-    });
-  }
-
-  ngOnInit() {
-    this.subscription = this.searchService.searchValue$.subscribe((value) => {
-      this.searchValue = value;
-      if (value) {
-        this.searchByCapital(value);
-      } else {
-        this.countries = [];
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
 }
--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TableComponent } from '../../components/table/table.component';
+import { Subscription } from 'rxjs';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
+import { SearchService } from '../../../components/services/search-box.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -7,5 +11,47 @@ import { TableComponent } from '../../components/table/table.component';
   imports: [TableComponent],
   templateUrl: './by-capital-page.component.html',
 })
-export class ByCapitalPageComponent {
+export class ByCapitalPageComponent implements OnDestroy, OnInit {
+  searchValue: string = '';
+  private subscription!: Subscription;
+
+  public loading: boolean = true;
+  public countries: Country[] = [];
+
+  constructor(
+    private countriesService: CountriesService,
+    private searchService: SearchService
+  ) {}
+
+  private searchByCapital(value: string): void {
+    this.loading = true;
+    this.countriesService.searchByCapital(value).subscribe({
+      next: (countries) => {
+        this.countries = countries;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error al buscar países:', error);
+        this.countries = [];
+        this.loading = false;
+      },
+    });
+  }
+
+  ngOnInit() {
+    this.subscription = this.searchService.searchValue$.subscribe((value) => {
+      this.searchValue = value;
+      if (value) {
+        this.searchByCapital(value);
+      } else {
+        this.countries = [];
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
