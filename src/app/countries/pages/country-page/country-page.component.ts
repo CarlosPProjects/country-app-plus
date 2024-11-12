0e934a212ece9d95b764ce2bdaca8fda6f5774b8
CarlosPProjects <cg9822@gmail.com>
2024-11-12 21:47:55 +0100

    Refactor table component to fix hover background color and add missing transition duration

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 import { Subscription, switchMap } from 'rxjs';
+import { LucideAngularModule, ChevronLeft, UsersRound } from 'lucide-angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-country-page',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule, RouterLink, CommonModule],
   templateUrl: './country-page.component.html',
 })
 export class CountryPageComponent implements OnInit, OnDestroy {
+
+  readonly chevronLeftIcon = ChevronLeft;
+  readonly usersRoundIcon = UsersRound;
+
   public country?: Country;
   private subscription!: Subscription;
 
